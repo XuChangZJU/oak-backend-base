4a2b215b7acca183c628ece8fbd4055d5bd96193
Xc@centOs <chinawraith@163.com>
2022-09-15 11:59:47 +0800

    1.0.5发布前更新依赖

--- lib/BackendContext.d.ts
+++
@@ -1,7 +0,0 @@
-import { RowStore } from 'oak-domain/lib/types';
-import { GeneralRuntimeContext } from 'oak-general-business';
-import { EntityDict } from 'oak-general-business/lib/general-app-domain';
-import { EntityDict as BaseEntityDict } from 'oak-domain/lib/base-app-domain';
-export declare class Context<ED extends EntityDict & BaseEntityDict> extends GeneralRuntimeContext<ED> {
-    static FromCxtStr(cxtStr?: string): <ED extends EntityDict & BaseEntityDict>(store: RowStore<ED, Context<ED>>) => Context<ED>;
-}
